69e051f617725b5ab4dca69ee7f2ce0a2fcc6edb
omkosalge <omkosalge7@gmail.com>
2026-09-07 11:27:21 +0000

    Create three EC2 instances using count

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,4 +13,23 @@ resource "aws_instance" "devops_terraform_control" {
     ManagedBy   = "Terraform"
     Project     = "aws-devops-practice"
   }
+
+  lifecycle {
+    ignore_changes = [
+      tags["Name"]
+    ]
+  }
+}
+resource "aws_instance" "devops_terraform_nodes" {
+  count = 3
+
+  ami           = "ami-05a1f40ec1f9ea141"
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "devops-terraform-node0${count.index + 1}"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+    Project     = "aws-devops-practice"
+  }
 }
