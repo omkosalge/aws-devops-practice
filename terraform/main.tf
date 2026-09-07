5cb8908bc30c9fbf69b3e6fb5a6ee75c733d65a1
omkosalge <omkosalge7@gmail.com>
2026-09-07 11:52:51 +0000

    for each

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,20 @@ moved {
   from = aws_instance.practice_server
   to   = aws_instance.devops_terraform_control
 }
+moved {
+  from = aws_instance.devops_terraform_nodes[0]
+  to   = aws_instance.devops_terraform_nodes["node01"]
+}
 
+moved {
+  from = aws_instance.devops_terraform_nodes[1]
+  to   = aws_instance.devops_terraform_nodes["node02"]
+}
+
+moved {
+  from = aws_instance.devops_terraform_nodes[2]
+  to   = aws_instance.devops_terraform_nodes["node03"]
+}
 resource "aws_instance" "devops_terraform_control" {
   ami           = "ami-05a1f40ec1f9ea141"
   instance_type = var.instance_type
@@ -24,13 +37,16 @@ resource "aws_instance" "devops_terraform_control" {
   }
 }
 resource "aws_instance" "devops_terraform_nodes" {
-  count = 3
-
+  for_each = toset([
+    "node01",
+    "node02",
+    "node03"
+  ])
   ami           = "ami-05a1f40ec1f9ea141"
   instance_type = var.instance_type
 
   tags = {
-    Name        = "devops-terraform-node0${count.index + 1}"
+    Name        = "devops-terraform-${each.value}"
     Environment = "dev"
     ManagedBy   = "Terraform"
     Project     = "aws-devops-practice"
